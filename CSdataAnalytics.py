5e05ee81a3b2da469094f72fe68c6b9a5b69fc9a
SvyatIva <151626994+SvyatIva@users.noreply.github.com>
2023-12-09 20:37:34 +0200

    complete rewrite for external saving and reading, whoops

--- CSdataAnalytics.py
+++ CSdataAnalytics.py
@@ -1,10 +1,47 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Dec  7 13:47:26 2023
+import json
+import os.path
 
-@author: Ilya
-"""
 
-a = input("How many games did you won per day? ")
-b = input("How many games did you lose per day? ")
-print("Your winrate: " + str(int(a)/(int(a)+int(b))*100) + "%");
+def stat_input(prompt):
+    # Repeat until a number is writen
+    while True:
+        val = input(prompt)
+        if val.isdigit():
+            val = int(val)
+            return val
+        print("[Invalid input.]")
+
+
+choice = ""
+print("------CS Stats Reader------")
+while choice != "0":
+    print("\n1 - read stats\n2 - write stats\n0 - exit\n")
+    choice = input("Your choice: ")
+
+    if choice == "1":
+        if not os.path.isfile("stats.json"):
+            print("No stats have been written yet.")
+        else:
+            with open("stats.json", "r") as outfile:
+                stats = json.load(outfile)
+            print("Games played: " + str(stats["games"]))
+            print("Kills performed: " + str(stats["kills"]))
+            print("Deaths occured: " + str(stats["deaths"]))
+
+    elif choice == "2":
+        # No idea what stats are there in cs, who knows please add more if needed
+        stats = {
+            "games": stat_input("Input the amount of games played: "),
+            "kills": stat_input("Input the amount of kills performed: "),
+            "deaths": stat_input("Input the amount of deaths that occurred: "),
+        }
+
+        with open("stats.json", "w") as outfile:
+            outfile.write(json.dumps(stats))
+
+        print()
+
+    elif choice != "0":
+        print("[Invalid choice.]")
+
+    print("\n---------------------------")
